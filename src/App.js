e94799db15728a5b591911613078bfe73bb29135
37231715 <37231715@student.g.nwu.ac.za>
2024-01-04 20:51:39 +0200

    Added area calculation in hectares

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 // import logo from './logo.svg';
 import './App.css';
-import React, { useState } from 'react'
-import { MapContainer, TileLayer, Marker, Popup, useMapEvents, Polyline} from 'react-leaflet';
+import React, { useDeferredValue, useEffect, useState } from 'react'
+import { MapContainer, TileLayer, Marker, Popup, useMapEvents, Polyline, Polygon} from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import Sidebar from './Components/sidebar.js';
 import L from 'leaflet';
 import pin from './images/pin.png';
-import pin2 from './images/pin2.png';
+import * as turf from '@turf/turf';
 
 const customMarkerIcon = L.icon({
   iconUrl: pin,
@@ -15,28 +15,75 @@ const customMarkerIcon = L.icon({
   popupAnchor: [0, -41]
 });
 
-function LocationMarker({ onDistanceChange }) {
+
+function LocationMarker({ mode, setMode, onDistanceChange, onAreaChange}) {
   const [markers, setMarkers] = useState([]);
 
   useMapEvents({
     click(e) {
-      setMarkers([...markers, e.latlng]);
-      if (markers.length > 1) {
-        const distance = calculateTotalDistance([...markers, e.latlng]);
-        onDistanceChange(distance);
+      // If in 'area' mode and the polygon is closed (last marker is same as first), insert new marker before last
+      if (mode === 'area' && markers.length >= 3 && markers[markers.length - 1] === markers[0]) {
+        const newMarkers = [...markers.slice(0, -1), e.latlng, markers[0]];
+        setMarkers(newMarkers);
+        const area = calculateTotalArea(newMarkers);
+        onAreaChange(area);
+      } else {
+        // If not closed or in 'distance' mode, just append the new marker
+        const newMarkers = [...markers, e.latlng];
+        setMarkers(newMarkers);
+        if (mode === 'distance') {
+          const distance = calculateTotalDistance(newMarkers);
+          onDistanceChange(distance);
+        } else if (mode === 'area' && markers.length >= 2) {
+          const area = calculateTotalArea(newMarkers);
+          onAreaChange(area);
+        }
       }
     }
   });
 
-  const updateMarkerPosition = (index, newLatLng) => {
-    const updatedMarkers = markers.map((marker, idx) => {
-      if (idx === index) {
-        return newLatLng;
+  useEffect(() => {
+    if (mode === 'area' && markers.length >= 3)
+    {
+      const lastMarker = markers[markers.length - 1];
+      const firstMarker = markers[0];
+      if (lastMarker !== firstMarker)
+      {
+        setMarkers([...markers, firstMarker]);
       }
-      return marker;
-    });
+    }
+    else if (mode === 'distance')
+    {
+      if (markers.length > 3 && markers[markers.length - 1] === markers[0])
+      {
+        setMarkers(markers.slice(0, -1));
+      }
+    }
+  }, [mode, markers]);
+
+  const updateMarkerPosition = (index, newLatLng) => {
+    let updatedMarkers = [...markers];
+    updatedMarkers[index] = newLatLng;
+  
+    // If the first marker is moved, update the last marker as well to keep the polygon closed
+    if (mode === 'area') {
+      if (index === 0 && updatedMarkers.length > 2) {
+        updatedMarkers[updatedMarkers.length - 1] = newLatLng;
+      }
+      // If the last marker is moved, update the first marker as well
+      else if (index === updatedMarkers.length - 1 && updatedMarkers.length > 2) {
+        updatedMarkers[0] = newLatLng;
+      }
+    }
+  
     setMarkers(updatedMarkers);
-    updateDistance(updatedMarkers);
+    // Update the distance or area after marker position change
+    if (mode === 'distance') {
+      updateDistance(updatedMarkers);
+    } else if (mode === 'area') {
+      const area = calculateTotalArea(updatedMarkers);
+      onAreaChange(area);
+    }
   };
 
   const removeMarker = (index) => {
@@ -52,7 +99,7 @@ function LocationMarker({ onDistanceChange }) {
   };
 
   const calculateTotalDistance = (points) => {
-    if (points.length < 2) {
+    if (points.length < 1) {
       return 0;
     }
 
@@ -61,6 +108,21 @@ function LocationMarker({ onDistanceChange }) {
       totalDistance += points[i].distanceTo(points[i + 1]);
     }
     return totalDistance / 1000;
+  };
+
+  const calculateTotalArea = (markers) => {
+    if (markers.length > 2) {
+      // Convert Leaflet LatLng objects to an array of [lng, lat] coordinates
+      const latlngs = markers.map(marker => [marker.lng, marker.lat]);
+      latlngs.push(latlngs[0]); // Close the polygon by duplicating the first point
+  
+      // Create a Turf polygon and calculate its area in square meters
+      const polygon = turf.polygon([latlngs]);
+      const area = turf.area(polygon) / 10000; // Convert to hectares
+  
+      return area.toFixed(2); // Return area in hectares with two decimals
+    }
+    return 0;
   };
 
   return (
@@ -79,7 +141,13 @@ function LocationMarker({ onDistanceChange }) {
           }}
           />
       )}
-      {markers.length > 1 && <Polyline positions={markers} />}
+
+      {markers.length > 2 && mode === 'area' && (
+        <Polygon positions={markers} />
+      )}
+
+      {markers.length > 1 &&  mode === 'distance' && (
+      <Polyline positions={markers} /> )}
     </>
   );
 }
@@ -92,7 +160,13 @@ function App() {
   const handleToggleSatellite = () => {};
   const handleToggleTerrain = () => {};
 
+  const [mode, setMode] = useState('distance'); // Distance or Area
   const [totalDistance , setTotalDistance] = useState(0);
+  const [totalArea, setTotalArea] = useState(0);
+
+  const handleModeChange = (newMode) => {
+    setMode(newMode);
+  }
 
   const handleDistanceChange = (distance) => {
     setTotalDistance(distance);
@@ -109,6 +183,7 @@ function App() {
       </button>
       <Sidebar
         isSidebarOpen={isSidebarOpen}
+        onModeChange={handleModeChange}
         onMeasureDistance={handleMeasureDistance}
         onMeasureArea={handleMeasureArea}
         onClearMap={handleClearMap}
@@ -116,14 +191,20 @@ function App() {
         onToggleTerrain={handleToggleTerrain}
       />
       <div className='distance-display'>
-        Total Distance: {totalDistance.toFixed(2)} km
+      {mode === 'distance' ? `Total Distance: ${totalDistance.toFixed(2)} km` :
+         `Total Area: ${totalArea} hectares`}
       </div>
       <MapContainer center={[-29.62, 24.08]} zoom={13} style={{ height: "100vh" }}>
         <TileLayer 
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         />
-        <LocationMarker onDistanceChange={handleDistanceChange} />
+        <LocationMarker 
+        mode={mode}
+        setMode={setMode}
+        onDistanceChange={handleDistanceChange}
+        onAreaChange={setTotalArea}
+        />
       </MapContainer>
     </div>
   );
--- src/Components/sidebar.js
+++ src/Components/sidebar.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './sidebar.css';
 
-function Sidebar({ isSidebarOpen, onMeasureDistance, onMeasureArea, onClearMap, onToggleSatellite, onToggleTerrain }) {
+function Sidebar({ onModeChange, isSidebarOpen, onMeasureDistance, onMeasureArea, onClearMap, onToggleSatellite, onToggleTerrain }) {
    return (
     <div className={`sidebar ${isSidebarOpen ? 'open' : ''}`}>
-        <button onClick={onMeasureDistance}>Measure Distance</button>
-        <button onClick={onMeasureArea}>Measure Area</button>
+        <button onClick={() => onModeChange('distance')}>Measure Distance</button>
+        <button onClick={() => onModeChange('area')}>Measure Area</button>
         <button onClick={onClearMap}>Clear Map</button>
         <button onClick={onToggleSatellite}>Satellite View</button>
         <button onClick={onToggleTerrain}>Terrain View</button>
